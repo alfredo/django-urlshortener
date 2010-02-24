c4091733a6c0703878ed32d7f120ad279e319078
Alfredo <aguirre.alfredo07@gmail.com>
2010-02-24 01:00:30 +0000

    tests

--- urlshortener/tests.py
+++ urlshortener/tests.py
@@ -1,23 +1,33 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from urlshortener.models import ShortUrl
+from django.conf import settings
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def setUp(self):
+        data = {'url': 'http://www.google.com',
+                'alias': 'a'}
+        self.short_url = ShortUrl.objects.create(**data)
 
->>> 1 + 1 == 2
-True
-"""}
+    def tearDown(self):
+        ShortUrl.objects.all().delete()
+
+    def testUrl(self):
+        """
+        Test that the url got created properly
+        """
+        result = ShortUrl.objects.all().count()
+        self.assertEquals(result, 1)
+        self.assertEquals(self.short_url.alias, 'a')
+        self.assertEquals(self.short_url.clicks, 0)
+
+    def testRedirection(self):
+        """
+        Tests it redirects properly
+        """
+        c = Client()
+        response = c.get('/a/', follow=True)
+        self.assertEquals(response.redirect_chain[0][0],
+                          'http://www.google.com')
 
